c2e1e1ccf96bd5d61914aef4151d4d77d39568c7
ronitgolan <User@DESKTOP-J4KKFIH>
2021-11-14 18:40:44 +0200

    fifth commit

--- RonitGolan.java
+++ RonitGolan.java
@@ -1,7 +1,7 @@
 public class RonitGolan{
     public static void main(String[] args) {
 	       for (int i=1;i<=10;i++){
-		                   System.out.println("hello world..."+"ro");
+		                   System.out.println("hello world..."+"r");
 						   
 		        }
 						   
