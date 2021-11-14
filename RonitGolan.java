dc6917d8b980e0f9e1324d825dd5c22f12c28dbe
ronitgolan <User@DESKTOP-J4KKFIH>
2021-11-14 18:22:04 +0200

    second commit

--- RonitGolan.java
+++ RonitGolan.java
@@ -1,7 +1,7 @@
 public class RonitGolan{
     public static void main(String[] args) {
 	       for (int i=1;i<=10;i++){
-		                   System.out.println("hello world..."+"ronit");
+		                   System.out.println("hello world..."+"roni");
 						   
 		        }
 						   
